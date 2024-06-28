2f7d8112ea68b2f6610a76fe4df7da6c37e57e60
AileenXMods <134466030+aileenxmods@users.noreply.github.com>
2024-06-28 09:18:24 +0700

    Update config.py

--- userbot/config.py
+++ userbot/config.py
@@ -4,11 +4,11 @@ DEVS = [
     6934282635
 ]
 
-API_ID = int(os.getenv("API_ID", "26522923"))
+API_ID = int(os.getenv("API_ID", "27934485"))
 
-API_HASH = os.getenv("API_HASH", "d3fb3a6e5c23345409a5e100175fca84")
+API_HASH = os.getenv("API_HASH", "922751167fa262b2fb1f6f23b595ed7b")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6536595465:AAEyjH8mRZGwJwPkK20ZMgTbSsYFM4_sk0M")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6536595465:AAG28m_S0eaopeU-dgxIgJE2S_NXZXcQnFM")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "6934282635"))
 
@@ -18,7 +18,7 @@ LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002170796011"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002170796011").split()))
 
-MAX_BOT = int(os.getenv("MAX_BOT", "40"))
+MAX_BOT = int(os.getenv("MAX_BOT", "50"))
 
 COMMAND = os.getenv("COMMAND", ".")
 PREFIX = COMMAND.split()
